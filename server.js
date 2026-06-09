2ebad18b71e393295f794e3c0d54f32eb2fe7fa9
Gajanan04 <shailamgg@gmail.com>
2026-06-09 13:33:22 +0530

    fixed cors issue

--- server.js
+++ server.js
@@ -9,45 +9,28 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(morgan("dev"));
 
-const corsOptions = {
-  origin: "https://taskflow-fullstack-app-green.vercel.app",
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
-
-app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://taskflow-fullstack-app-green.vercel.app"
-  );
-
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
-
-  next();
-});
-
+// Middleware
+app.use(cors());
 
 app.use(express.json());
+
+app.use(morgan("dev"));
+
+// Routes
 app.use("/api/v1/auth", require("./routes/authRoutes"));
+
 app.use("/api/v1/tasks", require("./routes/taskRoutes"));
 
+// Test Route
 app.get("/", (req, res) => {
   res.send("API Running...");
 });
 
+// Port
 const PORT = process.env.PORT || 5000;
 
+// Server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
